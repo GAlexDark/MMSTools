ba1c1be49154d58bd6792e50c833f261952b82d7
Oleksii Gaienko <galexsoftware@gmail.com>
2024-07-20 01:21:49 +0300

    Added the new System Log parser Class

---
+++ Common/Core/CSystemLogReport.cpp
@@ -0,0 +1,101 @@
+/****************************************************************************
+*
+*  Copyright (c) Oleksii Gaienko, 2024
+*  Contact: galexsoftware@gmail.com
+*
+*  Event Log Conversion Utility
+*  Common module
+*
+*  Module name: CSystemLogReport.cpp
+*  Author(s): Oleksii Gaienko
+*  Reviewer(s):
+*
+*  Abstract:
+*     The class for build the MMS System logs report.
+*
+****************************************************************************/
+
+#include "CSystemLogReport.h"
+#include "DBStrings.h"
+
+CSystemLogReport::CSystemLogReport(QObject *parent)
+    : CBasicReport{parent}
+{
+    clearErrorString();
+}
+
+bool
+CSystemLogReport::generateReport()
+{
+    bool retVal = true;
+    // Set datetime format
+    QXlsx::Format dateFormat;
+    setDateTimeFormat(dateFormat);
+
+    int colRowNumber = 1;
+    int colTimestamp = 2;
+    int colUsername = 3;
+    int colRole = 4;
+    int colCompanyname = 5;
+    int colSeverity = 6;
+    int colMessage = 7;
+
+    QScopedPointer<QXlsx::Document> xlsxReport(new QXlsx::Document);
+    int row = 1;
+    // Add header
+    QVariant writeValue;
+    try {
+        writeValue = QStringLiteral("№");
+        setReportDataItem(xlsxReport.data(), colRowNumber, row, writeValue);
+
+        writeValue = QStringLiteral("Відмітка часу (за Київським часом)");
+        setReportDataItem(xlsxReport.data(), colTimestamp, row, writeValue);
+        writeValue = QStringLiteral("Ім'я користувача");
+        setReportDataItem(xlsxReport.data(), colUsername, row, writeValue);
+        writeValue = QStringLiteral("Роль");
+        setReportDataItem(xlsxReport.data(), colRole, row, writeValue);
+        writeValue = QStringLiteral("Компанія");
+        setReportDataItem(xlsxReport.data(), colCompanyname, row, writeValue);
+        writeValue = QStringLiteral("Рівень");
+        setReportDataItem(xlsxReport.data(), colSeverity, row, writeValue);
+        writeValue = QStringLiteral("Повідомлення");
+        setReportDataItem(xlsxReport.data(), colMessage, row, writeValue);
+
+        ++row;
+        int multipartRowCount = getMultipartRowCount() - 1;
+        while (m_db->isNext()) {
+            setReportDataItem(xlsxReport.data(), colRowNumber, row, QVariant::fromValue(multipartRowCount + row));
+
+            writeValue = m_db->geValue("timestamp").toDateTime();
+            if (!xlsxReport->write(row, colTimestamp, writeValue, dateFormat)) {
+                throw "Write error";
+            }
+            setReportDataItem(xlsxReport.data(), "username1", colUsername, row);
+            setReportDataItem(xlsxReport.data(), "role", colRole, row);
+            setReportDataItem(xlsxReport.data(), "companyname", colCompanyname, row);
+            setReportDataItem(xlsxReport.data(), "severity", colSeverity, row);
+            setReportDataItem(xlsxReport.data(), "message", colMessage, row);
+            ++row;
+            if (row > maxRowsCount) {
+                break;
+            }
+        } // while
+    } catch (const char* ex) {
+        setErrorString(ex);
+        retVal = false;
+    }
+    if (retVal) {
+        const QString fileName = createReportFilename(row);
+        retVal = xlsxReport->saveAs(fileName);
+        if (!retVal) {
+            setErrorString(QStringLiteral("Error save report file"));
+        }
+    }
+    return retVal;
+}
+
+QString
+CSystemLogReport::selectString() const
+{
+    return systemlog::selectData;
+}
---
+++ Common/Core/CSystemLogReport.h
@@ -0,0 +1,39 @@
+/****************************************************************************
+*
+*  Copyright (c) Oleksii Gaienko, 2024
+*  Contact: galexsoftware@gmail.com
+*
+*  Event Log Conversion Utility
+*  Common module
+*
+*  Module name: CSystemLogReport.h
+*  Author(s): Oleksii Gaienko
+*  Reviewer(s):
+*
+*  Abstract:
+*     The class for build the MMS System logs report.
+*
+****************************************************************************/
+
+#ifndef CSYSTEMLOGREPORT_H
+#define CSYSTEMLOGREPORT_H
+
+#include "CBasicReport.h"
+
+class CSystemLogReport : public CBasicReport
+{
+    Q_OBJECT
+    Q_CLASSINFO("ID", "3") //4
+    Q_CLASSINFO("source", "systemlog")
+public:
+    Q_INVOKABLE explicit CSystemLogReport(QObject *parent = nullptr);
+    bool generateReport() override;
+    QString visibleReportName() override { return QObject::tr("System Log"); }
+    QString selectString() const override;
+};
+
+Q_DECLARE_METATYPE(CSystemLogReport *);
+
+typedef CSystemLogReport *pSystemLogReport;
+
+#endif // CSYSTEMLOGREPORT_H
