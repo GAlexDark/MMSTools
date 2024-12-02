8a897d462b25823ef2c852bcdcdb038a9d897fad
Oleksii Gaienko <galexsoftware@gmail.com>
2024-12-03 00:02:21 +0200

    Added repameters and comments

--- example.env.ps1
+++ example.env.ps1
@@ -1,7 +1,9 @@
+# The $PROXY_NAME variable value must be contains DND name only (without http:// and the slashes)
 [string] $PROXY_NAME = ""
 [string] $PROXY_PORT = ""
 
 [string] $REMOTE_HOST = "192.0.2.2"
+[int] $REMOTE_PORT = 22
 # The $REMOTE_PATH variable value must end with a '/' !!!!
 [string] $REMOTE_PATH = "/home/mr_data/upload/"
 
