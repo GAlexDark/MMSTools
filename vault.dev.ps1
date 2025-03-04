af98b7cbe0319f2b33751c20631c5d6f0f5e6d76
Oleksii Gaienko <galexsoftware@gmail.com>
2025-03-05 00:41:46 +0200

    Added p7bmaker v2 config example

---
+++ vault.dev.ps1
@@ -0,0 +1,5 @@
+[string] $LOCAL_STORAGE_MOUNT_POINT = "C"
+[UInt64] $LOCAL_STORAGE_MIN_SPACE = 10737418240 # 10G
+
+[string] $VAULT_ADDRESS = "http://vault.loc:8200"
+[string] $SECRET_NAME = "SECRET"
